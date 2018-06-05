ce3534f4aac6ad1a7359a863a0dd8c9b7eb04df5
lauret_s <seb.lauret@gmail.com>
2018-06-05 14:10:31 -0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <list>
+#include <unistd.h>
+#include "game_conf.hpp"
+#include "planet.hpp"
+#include "player.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
+  std::list<sf::Shape*> listDrawableElem;
+  Planet planet(500, 500, 100);
+  Player player(planet);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+  listDrawableElem.push_front(&player);
+  listDrawableElem.push_front(&planet);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      } else if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Left)
+          player.goLeft();
+        if (event.key.code == sf::Keyboard::Right)
+          player.goRight();
+      }
     }
-
-    return 0;
+    window.clear();
+    for (auto drawableElem: listDrawableElem) {
+      window.draw(*drawableElem);
+    }
+    window.display();
+    //sleep(1);
+  }
+  return 0;
 }
